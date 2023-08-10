b81c6682d410ddfbea87b29b2bee3161e2f0ce61
williambl <william@williambl.com>
2023-08-10 19:51:56 +0100

    fix: use lambda codec

--- src/main/java/com/williambl/dfunc/impl/DataFunctionsEnvironment.java
+++ src/main/java/com/williambl/dfunc/impl/DataFunctionsEnvironment.java
@@ -122,7 +122,8 @@ public class DataFunctionsEnvironment implements VEnvironment {
                                         .map(DataResult::success)
                                         .orElse(DataResult.error(() -> "Unmatched type!")),
                         Function.identity()),
-                ListConstructionDecoder.createCodec(this, spec, type)));
+                ListConstructionDecoder.createCodec(this, spec, type),
+                LambdaDecoder.createCodec(this, spec, type)));
     }
 
     @Override
